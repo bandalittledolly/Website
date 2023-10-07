a5433d14c3214f2009284088c10826bcebc07b1e
GeovaneDev <87013843+GeovaneDev@users.noreply.github.com>
2023-10-07 18:23:02 -0300

    Melhorias e Bugs corrigidos

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
-$(document).ready(function () {
-    $('.section-left').waypoint(function () {
-        $(this.element).addClass('animate__fadeInLeft').removeClass('invisible');
-    }, {
-        offset: '75%'
+function openFullscreenImage(imageUrl) {
+    var container = document.createElement("div");
+    container.style.position = "fixed";
+    container.style.top = "0";
+    container.style.left = "0";
+    container.style.width = "100%";
+    container.style.height = "100%";
+    container.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    container.style.display = "flex";
+    container.style.justifyContent = "center";
+    container.style.alignItems = "center";
+    container.style.cursor = "pointer";
+
+    var img = document.createElement("img");
+    img.src = imageUrl;
+    img.style.maxWidth = "90%"; 
+    img.style.maxHeight = "90vh";
+
+    container.addEventListener("click", function (event) {
+        if (event.target === container) {
+            document.body.removeChild(container);
+        }
     });
 
-    $('.section-right').waypoint(function () {
-        $(this.element).addClass('animate__fadeInRight').removeClass('invisible');
-    }, {
-        offset: '75%'
-    });
+    var closeButton = document.createElement("span");
+    closeButton.innerHTML = "&times;";
+    closeButton.style.position = "absolute";
+    closeButton.style.top = "20px";
+    closeButton.style.right = "20px";
+    closeButton.style.fontSize = "30px";
+    closeButton.style.color = "#fff";
+    closeButton.style.cursor = "pointer";
 
-    $('.section-up').waypoint(function () {
-        $(this.element).addClass('animate__fadeInUp').removeClass('invisible');
-    }, {
-        offset: '75%'
-    });
+    closeButton.onclick = function () {
+        document.body.removeChild(container);
+    };
 
-    $('.section-down').waypoint(function () {
-        $(this.element).addClass('animate__fadeInDown').removeClass('invisible');
-    }, {
-        offset: '75%'
+    container.appendChild(img);
+    container.appendChild(closeButton);
+    document.body.appendChild(container);
+}
+
+function isMobile() {
+    return window.innerWidth <= 768;
+}
+
+var images = document.querySelectorAll(".section-img, .img-fluid");
+images.forEach(function (image) {
+    image.addEventListener("click", function () {
+        var imageUrl = image.src;
+        openFullscreenImage(imageUrl);
+        if (isMobile()) {
+            document.querySelector("img[src='" + imageUrl + "']").style.maxWidth = "90%";
+            document.querySelector("img[src='" + imageUrl + "']").style.maxHeight = "90vh";
+        }
     });
 });
